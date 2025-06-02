84f07c995072d26dae5bf0c321a50fc499081279
drjaeger <lauris.mednieks@gmail.com>
2025-06-02 12:22:36 +0300

    Add workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
-            fallback: 'index.html'
+            fallback: 'index.html', // Ensures SPA fallback
         }),
-        paths: {
-            base: '/mana-base-helper'
-        },
-
-    }
+    },
 };
 
 export default config;
