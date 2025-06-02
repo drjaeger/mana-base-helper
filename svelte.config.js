e1ca2a5487f4ea6511916ca4139a7079ab355148
drjaeger <lauris.mednieks@gmail.com>
2025-06-02 12:33:23 +0300

    debug

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
-    kit: {
-        adapter: adapter({
-            fallback: 'index.html', // Ensures SPA fallback
-        }),
-    },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
