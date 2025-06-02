475168f90a7f43f91e5c832a2f1da8be5302ea09
drjaeger <lauris.mednieks@gmail.com>
2025-06-02 12:04:28 +0300

    Updates

---
+++ _app/immutable/chunks/CrQP86nV.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_tr9v23)==null?void 0:s.base)??"/mana-base-helper";var a;const t=((a=globalThis.__sveltekit_tr9v23)==null?void 0:a.assets)??e;export{t as a,e as b};
