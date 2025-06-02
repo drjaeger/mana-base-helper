e460cba39a0d190da4582b3733fdab8bd3ac5705
drjaeger <lauris.mednieks@gmail.com>
2025-06-02 11:55:18 +0300

    Updates

--- _app/immutable/chunks/N0lyWIGL.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_mhl8e)==null?void 0:s.base)??"/mana-base-helper";var e;const l=((e=globalThis.__sveltekit_mhl8e)==null?void 0:e.assets)??a;export{l as a,a as b};
